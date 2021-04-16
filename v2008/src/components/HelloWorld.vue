608bdfb7c608b0c85595529fb823eb1adc9dea6e
unknown <you@example.com>
2021-04-16 15:17:09 +0800

    4.16

---
+++ v2008/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <el-switch
+        v-model="value1"
+        active-text="按月付费"
+        inactive-text="按年付费">
+    </el-switch>
+    <el-switch
+        style="display: block"
+        v-model="value2"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        active-text="按月付费"
+        inactive-text="按年付费">
+    </el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
